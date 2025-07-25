22e0e561202bca72e4ed878f932ccdf5a48b5388
Pham Thao Ngoc <ngoc.phamthao@hcmut.edu.vn>
2025-07-25 11:47:43 +0700

    ps.ps1

--- ps.ps1
+++ ps.ps1
@@ -1,44 +1,15 @@
-# Path to sample file
-$basePath = "C:\Users\Public\Documents\SampleScript"
-New-Item -ItemType Directory -Path $basePath -Force | Out-Null
-
-# Create text message
-"Have a nice day" | Set-Content -Path "$basePath\message.txt"
-
-# Zip the file
-$zipPath = "$basePath\message.zip"
-Compress-Archive -Path "$basePath\message.txt" -DestinationPath $zipPath -Force
-
-# Send to Telegram
 $token = "8392772771:AAFgffIbtVeD4xBpg0rzlGSfPsbW0GG-a0o"
 $chatID = "7398435102"
+$zipPath = "C:\Users\Public\Documents\SampleScript\message.zip"
+
 $uri = "https://api.telegram.org/bot$token/sendDocument"
-$caption = "Have a nice day from PowerShell!"
 
-# Prepare HTTP request
-$client = New-Object System.Net.Http.HttpClient
-$content = New-Object System.Net.Http.MultipartFormDataContent
-$content.Add((New-Object System.Net.Http.StringContent($chatID)), "chat_id")
-$content.Add((New-Object System.Net.Http.StringContent($caption)), "caption")
-
-# Attach file
-$fileStream = [System.IO.File]::OpenRead($zipPath)
-$fileContent = New-Object System.Net.Http.StreamContent($fileStream)
-$fileContent.Headers.ContentType = [System.Net.Http.Headers.MediaTypeHeaderValue]::Parse("application/zip")
-$content.Add($fileContent, "document", "message.zip")
-
-# Send
-$response = $client.PostAsync($uri, $content).Result
-$responseBody = $response.Content.ReadAsStringAsync().Result
-$responseBody | Out-File -FilePath "$basePath\telegram_response.txt"
-
-# Cleanup
-$fileStream.Close()
-$fileStream.Dispose()
-
-# Caps Lock signal
-$keyBoardObject = New-Object -ComObject WScript.Shell
-for ($i=0; $i -lt 4; $i++) {
-    $keyBoardObject.SendKeys("{CAPSLOCK}")
-    Start-Sleep -Seconds 1
+$form = @{
+    chat_id = $chatID
+    caption = "Have a nice day from PowerShell!"
+    document = Get-Item $zipPath
 }
+
+$response = Invoke-RestMethod -Uri $uri -Method Post -Form $form
+
+$response | ConvertTo-Json
